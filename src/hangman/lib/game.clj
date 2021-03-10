d673e463e7accc6e48e57a652f7680ff744846f8
Roger E. Randall <roger.randall@gmail.com>
2021-03-09 22:25:59 -0500

    progress on process/evaluate-move - includes error commentary

--- src/hangman/lib/game.clj
+++ src/hangman/lib/game.clj
@@ -1,5 +1,6 @@
 (ns hangman.lib.game
-  (:require [clojure.string :as str]))
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
 
 (def game-state {:turns-remaining 7
                  :game-state :initializing
@@ -47,28 +48,40 @@
     false :good-guess
     true :won))
 
-;; Not Implemented
+;; FIXME:
+;; Created a new game with word `look`
+;; submitted make-move with two correct letters first (l,o)
+;; then when I submitted `z`, it blew up with "java.lang.IllegalArgumentException - No matching clause"
+;; call stack was (most recent on top):
+                  ;; game.clj:   52  hangman.lib.game/evaluate-guess
+                  ;; game.clj:   51  hangman.lib.game/evaluate-guess
+                  ;; game.clj:   72  hangman.lib.game/process-move
+                  ;; game.clj:   64  hangman.lib.game/process-move
+                  ;; game.clj:   83  hangman.lib.game/make-move
+                  ;; game.clj:   75  hangman.lib.game/make-move
+
 (defn evaluate-guess [game good-guess?]
-  ;; this might be better as an if-let or when-let
   (case good-guess?
     false (if (= 1 (:turns-remaining game))
             ;; if you make a bad guess (false) with 1 turn remaining, then you just lost
             (assoc game :turns-remaining 0 :game-state :lost)
-            ;; otherwise, you just lose a turn
+            ;; otherwise, you lose a turn
             (-> game
                 (assoc :game-state :bad-guess)
                 (update :turns-remaining dec)))
-    true nil))
+    true (let [letter-set (into #{} (:target-letters game))
+               won-game? (set/subset? letter-set (:already-guessed-letters game))]
+           (assoc game :game-state (did-guess-win-game won-game?)))))
 
 (defn process-move
   "If `guess` is `already-guessed` then player loses turn `(dec turns)`. Otherwise, `evaluate-guess`."
   [game guess already-guessed?]
   (case already-guessed?
-    ;; I don't know if this code works. Can I replace get-in -> conj -> assoc-in with update-in???
-    false (let [current-set (get-in game [:already-guessed-letters])
-                updated-set (conj current-set guess)
-                game (assoc-in game [:already-guessed-letters] updated-set)]
-            evaluate-guess game guess (contains? (:target-letters game) guess))
+    false (let [current-guesses (:already-guessed-letters game)
+                updated-guesses (conj current-guesses guess)
+                game (assoc-in game [:already-guessed-letters] updated-guesses)
+                good-guess? (some #(= guess %) (:target-letters game))]
+            (evaluate-guess game good-guess?))
     true (update game :turns-remaining dec)))
 
 (defn make-move
@@ -81,4 +94,6 @@
       :lost (return-tally game)
       (process-move game guess (contains? already-guessed guess)))))
 
-#_(new-game "sensibility")
+#_(def game (new-game "sensibility"))
+#_(def game (make-move game "s"))
+#_(def game (make-move game "i"))
