ee1f14344a5fbb9286d8f35f8a2010f5a2a1264f
Roger E. Randall <roger.randall@gmail.com>
2021-03-11 19:07:10 -0500

    implement some additional tests

--- test/hangman/lib/game_test.clj
+++ test/hangman/lib/game_test.clj
@@ -9,7 +9,7 @@
 (deftest new-game
   (testing "creates a new map representing the correct game state"
     (is (= {:turns-remaining 7
-            :game-state :initializing
+            :game-state :initialized
             :target-letters ["t" "e" "s" "t" "i" "n" "g"]
             :already-guessed-letters #{}}
            (game/new-game "testing")))))
@@ -23,3 +23,22 @@
   (testing "shows only the letters of word that have been guessed"
     (is (= '("_" "e" "s" "_") (game/reveal-target ["w" "e" "s" "t"] #{"e" "s"})))
     (is (= '("_" "_" "_" "_") (game/reveal-target ["w" "e" "s" "t"] #{})))))
+
+(deftest tally
+  (testing "returns game-state with unguessed letters hidden"
+    (is (= {:game-state :bad-guess
+            :turns-remaining 3
+            :revealed-portion-of-target '("t" "_" "s" "t" "_" "_" "_")}
+           (let [game {:turns-remaining 4
+                       :game-state :bad-guess
+                       :target-letters ["t" "e" "s" "t" "i" "n" "g"]
+                       :already-guessed-letters #{"t" "s" "z"}}]
+             (game/tally game))))))
+
+(deftest win-or-good-guess
+  (testing "changes :game-state if game is won/lost"
+    (is (= :good-guess (game/win-or-good-guess false)))
+    (is (= :won (game/win-or-good-guess true)))))
+
+;; This will run all tests automatically each timr buffer is reloaded `C-c C-k`
+(run-tests)
